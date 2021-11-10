2d43fdf77b6b979efa896ccdbc9b773e3b4c325f
Sedo Kray <sedokray@gmail.com>
2021-11-10 14:32:16 +0100

    fix script deploy with docker-compose

--- scripts/docker-compose/app-build-deploy.sh
+++ scripts/docker-compose/app-build-deploy.sh
@@ -4,5 +4,6 @@ mvn compile com.google.cloud.tools:jib-maven-plugin:2.3.0:dockerBuild
 echo "###### Build image store-app-ui"
 docker build -t store-app-ui:0.0.1-SNAPSHOT ./store-app-ui
 echo "###### Deploy store app using docker-compose"
+cd scripts/docker-compose
 docker-compose up -d
 # docker-compose down
