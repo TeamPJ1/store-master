cbffb64424a7713d60e965d5d27e9df6eb476644
Sedo Kray <sedokray@gmail.com>
2021-11-12 17:08:31 +0100

    fix K8s files

--- scripts/k8s/app-build-deploy.sh
+++ scripts/k8s/app-build-deploy.sh
@@ -5,4 +5,14 @@ echo "###### Build image store-app-ui"
 docker build -t store-app-ui:0.0.1-SNAPSHOT ./store-app-ui
 echo "###### Deploy store app using docker-compose"
 cd scripts/k8s
-kubectl apply -f  *.yml
+#kubectl apply -f ns.yml
+# Services in Kubernetes expose their endpoint using a common DNS pattern. It looks like this:
+# <Service Aame>.<Namespace Name>.svc.cluster.local
+kubectl create namespace storestg
+kubectl apply -f  eureka-server.yml -n storestg
+kubectl apply -f  config-server.yml -n storestg
+kubectl apply -f  cloud-gateway.yml -n storestg
+kubectl apply -f  inventory-service.yml -n storestg
+kubectl apply -f  store-app-ui.yml -n storestg
+kubectl apply -f  ingress.yml -n storestg
+
