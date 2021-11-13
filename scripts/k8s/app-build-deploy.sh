0a3fde3bca5f255e24f4ce75744317b1650bfe34
Sedo Kray <sedokray@gmail.com>
2021-11-13 09:43:54 +0100

    modify script deploy with k8S

--- scripts/k8s/app-build-deploy.sh
+++ scripts/k8s/app-build-deploy.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
+while getopts n:s: flag
+do
+    case "${flag}" in
+        n) namespace=${OPTARG};;
+        s) state=${OPTARG};;
+    esac
+done
+echo "Namespace: $namespace";
+echo "State: $state";
+
 echo "###### Build image backend services"
 mvn compile com.google.cloud.tools:jib-maven-plugin:2.3.0:dockerBuild
 echo "###### Build image store-app-ui"
 docker build -t store-app-ui:0.0.1-SNAPSHOT ./store-app-ui
-echo "###### Deploy store app using docker-compose"
+
 cd scripts/k8s
 #kubectl apply -f ns.yml
 # Services in Kubernetes expose their endpoint using a common DNS pattern. It looks like this:
 # <Service Aame>.<Namespace Name>.svc.cluster.local
 
-kubectl create namespace storestg
-#kubectl apply -f  eureka-server.yml -n storestg
-kubectl create -f  eureka-server.yml -n storestg
-kubectl create -f  config-server.yml -n storestg
-kubectl create -f  cloud-gateway.yml -n storestg
-kubectl create -f  inventory-service.yml -n storestg
-kubectl create -f  store-app-ui.yml -n storestg
-kubectl create -f  ingress.yml -n storestg
+echo "###### Delete existed resource store app"
+kubectl delete -f  eureka-server.yml -n $namespace
+kubectl delete -f  config-server.yml -n $namespace
+kubectl delete -f  cloud-gateway.yml -n $namespace
+kubectl delete -f  inventory-service.yml -n $namespace
+kubectl delete -f  store-app-ui.yml -n $namespace
+kubectl delete -f  ingress.yml -n $namespace
+kubectl delete namespace $namespace --force
+echo "###### Deploy store app using docker-compose"
+kubectl create namespace $namespace
+#kubectl apply -f  eureka-server.yml -n $namespace
+kubectl create -f  eureka-server.yml -n $namespace
+kubectl create -f  config-server.yml -n $namespace
+kubectl create -f  cloud-gateway.yml -n $namespace
+kubectl create -f  inventory-service.yml -n $namespace
+kubectl create -f  store-app-ui.yml -n $namespace
+kubectl create -f  ingress.yml -n $namespace
