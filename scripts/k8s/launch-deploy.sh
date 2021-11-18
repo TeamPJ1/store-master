52fbb32d17447a0083aaacef9b73319eb226dd00
Sedo Kray <sedokray@gmail.com>
2021-11-18 14:03:20 +0100

    test deploy k8s

--- scripts/k8s/launch-deploy.sh
+++ scripts/k8s/launch-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo rm -rf store-master
-git clone https://github.com/TeamPJ1/store-master.git
+git clone -b main https://github.com/TeamPJ1/store-master.git
 cd store-master
 chmod a+x scripts/k8s/app-build-deploy.sh
 sudo ./scripts/k8s/app-build-deploy.sh -n storestg  -b true -s start -i 192.168.12.12
